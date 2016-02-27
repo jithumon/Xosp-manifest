ba59ba8db1ea132c540b9d10ab2a38a6145a3202
Tushar Jain <catusharjain95@gmail.com>
2016-02-27 22:39:06 +0530

    Update cloner.sh

--- cloner.sh
+++ cloner.sh
@@ -4,7 +4,7 @@ rm -rf device/oppo/common
 rm -rf device/qcom/common
 rm -rf kernel/oneplus/msm8974
 rm -rf vendor/oneplus
-git clone https://github.com/darkobas/android_device_oneplus_bacon.git -b M-caf device/oneplus/bacon
+git clone https://github.com/tusharjain95/android_device_oneplus_bacon.git -b aosp device/oneplus/bacon
 git clone https://github.com/darkobas/android_kernel_oneplus_msm8974.git -b M-caf-remix kernel/oneplus/msm8974
 git clone https://github.com/darkobas/proprietary_vendor_oneplus.git -b M-caf vendor/oneplus
 git clone https://github.com/darkobas/android_device_qcom_msm8974.git -b M-caf device/qcom/msm8974
