c0c6baac825bd7dcf049a2d02d3e8eb400b79def
Tushar Jain <catusharjain95@gmail.com>
2016-03-07 14:14:23 +0530

    Update cloner.sh

--- cloner.sh
+++ cloner.sh
@@ -4,7 +4,7 @@ rm -rf device/oppo/common
 rm -rf device/qcom/common
 rm -rf kernel/oneplus/msm8974
 rm -rf vendor/oneplus
-git clone https://github.com/CyanogenMod/android_kernel_oneplus_msm8994.git -b cm-13.0 kernel/oneplus/msm8974
+git clone https://github.com/CyanogenMod/android_kernel_oneplus_msm8974.git -b cm-13.0 kernel/oneplus/msm8974
 git clone https://github.com/CyanogenMod/android_device_oneplus_bacon.git -b cm-13.0 device/oneplus/bacon
 git clone https://github.com/CyanogenMod/android_device_qcom_common.git -b cm-13.0 device/qcom/common
 git clone https://github.com/CyanogenMod/android_device_oppo_common.git -b cm-13.0 device/oppo/common
