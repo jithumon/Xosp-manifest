336d4fb40319c406d2b190ee283b645bfb015ce8
Tushar Jain <catusharjain95@gmail.com>
2016-04-23 16:23:29 +0530

    Update cloner.sh

--- cloner.sh
+++ cloner.sh
@@ -4,9 +4,20 @@ rm -rf device/oppo/common
 rm -rf device/qcom/common
 rm -rf kernel/oneplus/msm8974
 rm -rf vendor/oneplus
-git clone https://github.com/tusharjain95/android_device_oneplus_bacon.git -b aosp device/oneplus/bacon
-git clone https://github.com/darkobas/android_kernel_oneplus_msm8974.git -b M-caf-remix kernel/oneplus/msm8974
-git clone https://github.com/darkobas/proprietary_vendor_oneplus.git -b M-caf vendor/oneplus
-git clone https://github.com/darkobas/android_device_qcom_msm8974.git -b M-caf device/qcom/msm8974
-git clone https://github.com/darkobas/android_device_qcom_common.git -b M-caf device/qcom/common
+rm -rf hardware/qcom/audio
+rm -rf hardware/qcom/media
+rm -rf hardware/qcom/display
+rm -rf hardware/ril
+rm -rf packages/apps/Snap
+
+git clone https://github.com/sultanxda/android_device_oneplus_bacon.git -b cm-13.0-caf device/oneplus/bacon
+git clone https://github.com/sultanxda/android_kernel_oneplus_msm8974.git -b cm-13.0-caf kernel/oneplus/msm8974
+git clone https://github.com/sultanxda/proprietary_vendor_oneplus.git -b cm-13.0-caf vendor/oneplus
+git clone https://github.com/Cyanogenmod/android_device_qcom_common.git -b cm-13.0 device/qcom/common
 git clone https://github.com/Cyanogenmod/android_device_oppo_common.git -b cm-13.0 device/oppo/common
+git clone https://github.com/sultanxda/android_external_sony_boringssl-compat.git -b cm-13.0 external/sony/boringssl-compat
+git clone https://github.com/sultanxda/android_hardware_qcom_audio.git -b cm-13.0-caf hardware/qcom/audio
+git clone https://github.com/sultanxda/android_hardware_qcom_display.git -b cm-13.0-caf hardware/qcom/display
+git clone https://github.com/sultanxda/android_hardware_qcom_media.git -b cm-13.0-caf hardware/qcom/media
+git clone https://github.com/sultanxda/android_hardware_ril.git -b cm-13.0-legacy hardware/ril
+git clone https://github.com/sultanxda/android_packages_apps_Snap.git -b cm-13.0-sultan packages/apps/Snap
