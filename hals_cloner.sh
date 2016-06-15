ec018f6db82a0002af57a8f0d6449278f42d34d3
Tushar Jain <catusharjain95@gmail.com>
2016-06-15 13:59:51 +0530

    Create hals_cloner.sh

---
+++ hals_cloner.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/CyanogenMod/android_hardware_qcom_display -b cm-13.0-caf-8974 hardware/qcom/display-caf/msm8974
+git clone https://github.com/CyanogenMod/android_hardware_qcom_audio -b cm-13.0-caf-8974 hardware/qcom/audio-caf/msm8974
+git clone https://github.com/CyanogenMod/android_hardware_qcom_media -b cm-13.0-caf-8974 hardware/qcom/media-caf/msm8974
+git clone https://github.com/CyanogenMod/android_vendor_qcom_opensource_dataservices.git -b cm-13.0 vendor/qcom/opensource/dataservices
