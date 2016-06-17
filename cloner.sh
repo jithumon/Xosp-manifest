9b9a9c8d649fa5374300c1eedc5af1cdd97fa79d
Jithu Mathew Joseph <jithu311990@gmail.com>
2016-06-17 23:58:47 +0530

    Update cloner.sh

--- cloner.sh
+++ cloner.sh
@@ -5,7 +5,7 @@ rm -rf device/qcom/common
 rm -rf kernel/oneplus/msm8974
 rm -rf vendor/oneplus
 git clone https://github.com/CyanogenMod/android_kernel_oneplus_msm8974.git -b cm-13.0 kernel/oneplus/msm8974
-git clone https://github.com/CyanogenMod/android_device_oneplus_bacon.git -b cm-13.0 device/oneplus/bacon
+git clone https://github.com/jithumon/android_device_oneplus_bacon.git -b cm-13.0 device/oneplus/bacon
 git clone https://github.com/CyanogenMod/android_device_qcom_common.git -b cm-13.0 device/qcom/common
 git clone https://github.com/CyanogenMod/android_device_oppo_common.git -b cm-13.0 device/oppo/common
 git clone https://github.com/TheMuppets/proprietary_vendor_oneplus.git -b cm-13.0 vendor/oneplus
