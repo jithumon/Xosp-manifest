7f15af471bf9d89c00c712f6b85afa39a9ed53d7
Jithu Mathew Joseph <jithu311990@gmail.com>
2016-06-17 23:44:42 +0530

    Update cloner.sh

--- cloner.sh
+++ cloner.sh
@@ -4,20 +4,12 @@ rm -rf device/oppo/common
 rm -rf device/qcom/common
 rm -rf kernel/oneplus/msm8974
 rm -rf vendor/oneplus
-rm -rf hardware/qcom/audio
-rm -rf hardware/qcom/media
-rm -rf hardware/qcom/display
-rm -rf hardware/ril
-rm -rf packages/apps/Snap
+
 
 git clone https://github.com/jithumon/android_device_oneplus_bacon.git -b cm-13.0 device/oneplus/bacon
 git clone https://github.com/CyanogenMod/android_kernel_oneplus_msm8974.git -b cm-13.0 kernel/oneplus/msm8974
-git clone https://github.com/sultanxda/proprietary_vendor_oneplus.git -b cm-13.0-sultan vendor/oneplus
+git clone https://github.com/TheMuppets/proprietary_vendor_oneplus.git -b cm-13.0 vendor/oneplus
 git clone https://github.com/Cyanogenmod/android_device_qcom_common.git -b cm-13.0 device/qcom/common
 git clone https://github.com/Cyanogenmod/android_device_oppo_common.git -b cm-13.0 device/oppo/common
-git clone https://github.com/sultanxda/android_external_sony_boringssl-compat.git -b cm-13.0 external/sony/boringssl-compat
-git clone https://github.com/sultanxda/android_hardware_qcom_audio.git -b cm-13.0-caf hardware/qcom/audio
-git clone https://github.com/sultanxda/android_hardware_qcom_display.git -b cm-13.0-caf hardware/qcom/display
-git clone https://github.com/sultanxda/android_hardware_qcom_media.git -b cm-13.0-caf hardware/qcom/media
-git clone https://github.com/sultanxda/android_hardware_ril.git -b cm-13.0-legacy hardware/ril
-git clone https://github.com/sultanxda/android_packages_apps_Snap.git -b cm-13.0-sultan packages/apps/Snap
+git clone https://github.com/CyanogenMod/android_external_sony_boringssl-compat.git -b cm-13.0 external/sony/boringssl-compat
+
