f7c9bfc6a05ce2a61cccfd890e0b5d0b0c8f9f98
Jithu Mathew Joseph <jithu311990@gmail.com>
2016-07-15 01:13:56 +0530

    Update hals_cloner.sh

--- hals_cloner.sh
+++ hals_cloner.sh
@@ -2,4 +2,6 @@ git clone https://github.com/CyanogenMod/android_hardware_qcom_display -b cm-13.
 git clone https://github.com/CyanogenMod/android_hardware_qcom_audio -b cm-13.0-caf-8996 hardware/qcom/audio-caf/msm8996
 git clone https://github.com/CyanogenMod/android_hardware_qcom_media -b cm-13.0-caf-8996 hardware/qcom/media-caf/msm8996
 git clone https://github.com/CyanogenMod/android_hardware_qcom_bt -b cm-13.0-caf hardware/qcom/bt
+git clone https://github.com/CyanogenMod/android_hardware_ril-caf -b cm-13.0-caf hardware/ril-caf
+https://github.com/CyanogenMod/android_hardware_ril-caf.git
 git clone https://github.com/CyanogenMod/android_vendor_qcom_opensource_dataservices.git -b cm-13.0 vendor/qcom/opensource/dataservices
